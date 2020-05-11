a3bfce30ed6710d4d93b2aa64078467730a9045a
Seaman <kls190004@cvltechs-MacBook-Pro.local>
2020-05-11 17:50:08 -0500

    Update manipulation check

--- 01_manipulation_check.R
+++ 01_manipulation_check.R
@@ -27,4 +27,30 @@ d1 <- dt %>%
          socialcompare = socialcheck - SC5, 
          moneycompare = moneycheck - SC3, 
          healthcompare = healthcheck - SC4)
-         )
+
+# double check the rowsums function
+d2 <- dt[c(1, moneystart:moneyend)]
+money <- rowSums(d2[2:43])
+
+d3 <- dt[c(1, healthstart:healthend)]
+health <- rowSums(d3[2:43])
+
+
+# start from raw data
+raw <- read.csv(here::here('data', 'Time+discounting+across+domains_May+7,+2020_09.54.csv'))
+moneystart <- grep('Q80', colnames(raw))
+moneyend <- grep('Q121', colnames(raw))
+d2 <- raw[c(moneystart:moneyend)]
+d2 <- d2[-c(1:2),]
+
+d2[] <- lapply(d2, function(x) as.numeric(as.character(x)))
+money <- rowSums(d2[1:42])
+
+
+healthstart <- grep('Q125', colnames(raw))
+healthend <- grep('Q166', colnames(raw))
+d3 <- raw[c(healthstart:healthend)]
+d3 <- d3[-c(1:2),]
+
+d3[] <- lapply(d3, function(x) as.numeric(as.character(x)))
+health <- rowSums(d3[1:42])
