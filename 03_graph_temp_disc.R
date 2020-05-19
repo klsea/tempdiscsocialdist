e5e6a2077b025a43769c55c35f5393693f9c38c2
Seaman <kls190004@cvltechs-MacBook-Pro.local>
2020-05-19 13:31:43 -0500

    markdown report for temporal discounting

--- 03_graph_temp_disc.R
+++ 03_graph_temp_disc.R
@@ -8,6 +8,7 @@ library(ggplot2)
 
 # load source functions
 source(here::here("scr", "td_wide_to_long.R"))
+source(here::here("scr", "SummarySE.R"))
 
 # set hard-coded variables
 
@@ -16,9 +17,14 @@ dt <- read.csv(here::here("data", "tdsd_s1_data.csv"))
 
 d1 <- td_wide_to_long(dt)
 
-# graph
-ggplot(d1, aes(Age, propChoice, color = domain, fill = domain)) + geom_point() + geom_smooth(method=lm) + 
-  geom_smooth(method = "lm", formula = y ~ x + I(x^2), color = "black", fill = "black") +
-  facet_grid(.~ domain) +theme_minimal()
+# graph td x age
+td_x_age <- ggplot(d1, aes(Age, propChoice)) + geom_point(aes(color = domain)) + geom_smooth(method=lm, color = "black") + 
+  geom_smooth(method = "lm", formula = y ~ x + I(x^2), aes(color = domain, fill = domain )) +
+  facet_grid(.~ domain) +theme_minimal() + theme(legend.position="none") + 
+  ylab('Proportion of Smaller, Sooner Choices')
+td_x_age
 
-
+# graph td means
+td_means <- ggplot(d1, aes(domain, propChoice)) + geom_violin(trim = FALSE, aes(fill = domain)) + 
+ geom_boxplot(width=0.1, fill = "white") + theme_minimal() + theme(legend.position = "none")
+td_means
