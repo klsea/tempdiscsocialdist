a92874ca6ea5ce7208e7543e0e5ca374255ec32e
Seaman <kls190004@cvltechs-MacBook-Pro.local>
2020-05-28 17:04:59 -0500

    Add follow-ups about social partners and money use

--- 04_graph_temp_disc.R
+++ 04_graph_temp_disc.R
@@ -5,6 +5,7 @@
 library(here)
 library(tidyverse)
 library(ggplot2)
+library(plyr)
 
 # load source functions
 source(here::here("scr", "td_wide_to_long.R"))
@@ -14,6 +15,7 @@ source(here::here("scr", "SummarySE.R"))
 
 # load data
 dt <- read.csv(here::here("data", "tdsd_s1_data.csv"))
+dd <- read.csv(here::here("data", 'tdsd_s1_data_dictionary.csv'), stringsAsFactors=FALSE)
 
 d1 <- td_wide_to_long(dt)
 
@@ -28,3 +30,40 @@ td_x_age
 td_means <- ggplot(d1, aes(domain, propChoice)) + geom_violin(trim = FALSE, aes(fill = domain)) + 
  geom_boxplot(width=0.1, fill = "white") + theme_minimal() + theme(legend.position = "none")
 td_means
+
+# graph who is social partner
+dt$socialpartner <- dt$Q32
+dt$socialpartner <- mapvalues(dt$socialpartner, from = c(1,2,3,4,7,8,9), to = c('My spouse or significant other', 'My child', 'My parent', 
+'My grandparent or other relative', 'My close friend', 'A new friend', 'My co-worker'))
+part_freq <- count(dt$socialpartner) # count education levels in new table
+part_freq$socialpartner <- part_freq$x
+partplot <- ggplot(part_freq, aes(x='', y=freq, fill = socialpartner)) + geom_bar(stat = 'identity', width = 1) +
+  coord_polar("y", start = 0) + theme_void()
+partplot
+rm(part_freq)
+
+# graph where money is spent
+d2 <- dt[c(1, 184)]  %>% separate(Q170, as.character(1:6)) 
+colnames(d2) <- c('ID', paste0('C', colnames(d2[2:7])))
+
+d2 <- d2 %>%
+  mutate(M1 = ifelse(C1 == 1, 1, 0), 
+         M2 = ifelse(C1 == 2 | C2 == 2, 1, 0),
+         M3 = ifelse(C1 == 3 | C2 == 3 | C3 == 3, 1, 0), 
+         M4 = ifelse(C1 == 4 | C2 == 4 | C3 == 4 | C4 == 4, 1, 0), 
+         M5 = ifelse(C1 == 5 | C2 == 5 | C3 == 5 | C4 == 5 | C5 == 5, 1, 0), 
+         M6 = ifelse(C1 == 6 | C2 == 6 | C3 == 6 | C4 == 6 | C5 == 6 | C6 == 6, 1, 0)) %>%
+  mutate_at(vars(paste0("M", 1:6)), as.numeric) %>%
+  mutate_at(vars(paste0("M", 1:6)), replace_na, 0)
+
+d3 <- as.data.frame(colSums(d2[8:13]))
+colnames(d3) <- 'count'
+d3$expense <- c('Bills / essential items', 
+                  'Non-essential items', 'For someone else', 
+                  'Giving to someone else', 'Shared experience', 'Other')
+d3$expense <- ordered(d3$expense, levels = c('Bills / essential items', 
+                                             'Non-essential items', 'For someone else', 
+                                             'Giving to someone else', 'Shared experience', 'Other'))
+moneyuseplot <- ggplot(d3, aes(expense, count, fill = expense)) + geom_bar(stat = 'identity') + theme_minimal() + 
+  theme(legend.position='none', axis.text.x  = element_text(angle=90, vjust=0.5, size = 10))
+moneyuseplot
