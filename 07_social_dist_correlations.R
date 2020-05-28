1e88da5d4a7f1c13f02a7fa51cc4e2b41d79e390
Seaman <kls190004@cvltechs-MacBook-Pro.local>
2020-05-28 12:21:19 -0500

    Temp disc and social dist correlations markdown

--- 07_social_dist_correlations.R
+++ 07_social_dist_correlations.R
@@ -16,14 +16,17 @@ source(here::here("scr", "corrTableCI.R"))
 dt <- read.csv(here::here("data", "tdsd_s1_data.csv"))
 dd <- read.csv(here::here("data", 'tdsd_s1_data_dictionary.csv'), stringsAsFactors=FALSE)
 
+# add social/health difference score
+dt$discountdiff <- dt$SC5 - dt$SC4
+
 # correlation table for social distancing variables
 # formula removes all people with NA values, so analyze Q16, Q20 & Q22 separately
                             
-table1 <- corrTableCI(dt[c(6, 11:15, 24, 39:41, 186:188)])
+table1 <- corrTableCI(dt[c(6, 11:15, 24, 39:41, 186:192)])
 
-table2 <- corrTableCI(dt[c(6, 17:22)])
+table2 <- corrTableCI(dt[c(6, 17:22, 189:192)])
 
-table3 <- corrTableCI(dt[c(6, 26:31)])
+table3 <- corrTableCI(dt[c(6, 26:31, 189:192)])
 
-table4 <- corrTableCI(dt[c(6, 33:37)])
+table4 <- corrTableCI(dt[c(6, 33:37, 189:192)])
 
